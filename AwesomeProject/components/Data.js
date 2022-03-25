28a7c2530b082ea783cfdfa85ff84465c54ef38c
codergalib20 <coderboygalif@gmail.com>
2022-03-25 16:20:43 +0600

    commited after something change

--- AwesomeProject/components/Data.js
+++ AwesomeProject/components/Data.js
@@ -1,25 +1,42 @@
+import { Button } from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 class Data extends Component {
-  state = {
-    country: "Bangladesh",
-    age: 16,
-    city: "Nilphamari"
+  constructor() {
+    super();
+    this.state = {
+      country: "Bangladesh",
+      age: 16,
+      city: "Nilphamari",
+      count: 0,
+    }
   }
+  changing = () => {
+    this.setState({ country: "India", count: + 1 })
+  }
+ 
   render() {
     return (
       <View>
         <Text style={styles.text}>{this.state.country} || {this.state.age} || {this.state.city}</Text>
+        <Text style={styles.text2} >{this.state.count}</Text>
+        <Button onPress={this.changing} minH={20} >Button</Button>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   text: {
-      color: "#fff",
-      textAlign: 'center',
-      paddingVertical: 5,
-      backgroundColor: "#581845",
+    color: "#fff",
+    textAlign: 'center',
+    paddingVertical: 5,
+    backgroundColor: "#581845",
+  },
+  text2 : {
+    fontSize: 22,
+    color: "#581845",
+    textAlign: 'center',
+    paddingVertical: 5,
   }
 })
 
